3518f539afd7d935fad185ee7ee1446dc41fcb42
venkateshpolepally <venkateshpolepally@192.168.0.17>
2019-10-14 06:04:00 -0700

    wrapper class

--- src/OOPS_learning/wrapperClasses.java
+++ src/OOPS_learning/wrapperClasses.java
@@ -3,6 +3,14 @@ package OOPS_learning;
 public class wrapperClasses {
 
 	public static void main(String[] args) {
+		int x = 5;
+		
+		Integer val = x;//auto boxing
+		x = val;// auto un boxing converting from Integer to int
+		
+		String xyz = "1234";
+		int str = Integer.parseInt(xyz);//can use lotta such functions
+		System.out.println(Integer.toBinaryString(str));//these as well
 		
 
 	}
